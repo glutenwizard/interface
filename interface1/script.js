bcfde305a40abb0b6722c76c76808ef6e3410a5c
glutenwizard <111948759+glutenwizard@users.noreply.github.com>
2022-10-24 10:29:54 -0500

    Add files via upload

---
+++ interface1/script.js
@@ -0,0 +1,16 @@
+$(".text").hide();
+
+$(".topButton").click(function(){
+    $(".text").fadeIn(1000);
+});
+
+$(".textNanaK").hide();
+
+$(".imgNanaK").click(function(){
+    $(".textNanaK").fadeIn(1000);
+});
+
+$(".text").click(function(){
+    $(".text").hide();
+});
+
